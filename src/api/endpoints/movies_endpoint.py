aa238e9b9aa3347e8167db2bf73a1e5ff5b4445f
Denis AKPAGNONITE <akpagnonited@gmail.com>
2023-10-19 19:40:11 +0100

    refactor services

--- src/api/endpoints/movies_endpoint.py
+++ src/api/endpoints/movies_endpoint.py
@@ -1,7 +1,7 @@
 import requests
 from fastapi import APIRouter
 from starlette.status import HTTP_500_INTERNAL_SERVER_ERROR
-from typing import Any
+from typing import Any, List
 
 from src.services.movies_service import (
     load_movies,
@@ -12,11 +12,12 @@ from src.services.movies_service import (
 )
 from src.core.exceptions.api_exception import ApiException
 from src.core.json.rapid_json import RapidJSONResponse
+from src.api.schemas.movies_schema import Movie, MovieDetail, MovieByDate
 
 router = APIRouter()
 
 
-@router.get("/{theater}", tags=["Movies"], response_model=Any)
+@router.get("/{theater}", tags=["Movies"], response_model=List[Movie])
 def load_movies_endpoint(theater: str):
     try:
         response = load_movies(theater)
@@ -32,7 +33,7 @@ def load_movies_endpoint(theater: str):
         )
 
 
-@router.get("/info/slug", tags=["Movies"], response_model=Any)
+@router.get("/{slug}/info", tags=["Movies"], response_model=MovieDetail)
 def load_movie_info_endpoint(slug: str):
     try:
         response = load_movie_info(slug)
@@ -48,7 +49,7 @@ def load_movie_info_endpoint(slug: str):
         )
 
 
-@router.get("/movies-per-day/{theater}", tags=["Movies"], response_model=Any)
+@router.get("/{theater}/movies-per-day", tags=["Movies"], response_model=Any)
 def load_movies_per_day_endpoint(theater: str):
     try:
         response = load_movies_per_day(theater)
@@ -64,7 +65,11 @@ def load_movies_per_day_endpoint(theater: str):
         )
 
 
-@router.get("/display-by-date/{theater}/{date}", response_model=Any, tags=["Movies"])
+@router.get(
+    "/{theater}/display-by-date/{date}",
+    response_model=List[MovieByDate],
+    tags=["Movies"],
+)
 def display_movies_by_date_endpoint(theater: str, date: str):
     try:
         response = display_movies_by_date(theater, date)
@@ -81,7 +86,7 @@ def display_movies_by_date_endpoint(theater: str, date: str):
 
 
 @router.get(
-    "/display-by-language/{theater}/{language}", response_model=Any, tags=["Movies"]
+    "/{theater}/display-by-language/{language}", response_model=Any, tags=["Movies"]
 )
 def display_movies_by_date_endpoint(theater: str, language: str):
     try:
--- src/services/movies_service.py
+++ src/services/movies_service.py
@@ -1,48 +1,70 @@
 from collections import defaultdict
+from typing import List
+import logging
+
+from starlette.status import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
 
 from src.wraper.request_wraper import load_html_data, host
+from src.api.schemas.movies_schema import Movie, MovieDetail, MovieByDate
+from src.core.exceptions.api_exception import ApiException, ApiErrorsCode
 
 
-def load_movies(theater: str):
+LOGGER = logging.getLogger(__name__)
+
+
+def load_movies(theater: str) -> List[Movie]:
     """
     Load movies of a theater
-    :param theater:
-    :return:
+    :param theater: slug
+    :return: List af movie
     """
-    resource = (
-        f"{host}theaters/{theater}/"
-        if "activites-" not in theater
-        else f"{host}theaters/{theater.split('-')[-1]}"
-    )
+    try:
+        resource = (
+            f"{host}theaters/{theater}/"
+            if "activites-" not in theater
+            else f"{host}theaters/{theater.split('-')[-1]}"
+        )
 
-    response = []
+        response: List[Movie] = []
 
-    soup = load_html_data(resource)
-    theater_movies = soup.find_all(
-        "ul", class_="theater-movies", attrs={"data-date": True}
-    )
-    for theater_movie in theater_movies:
-        date_value = theater_movie["data-date"]
-        movies = theater_movie.find_all("li", class_="")
-        for movie in movies:
-            movie_url = movie.find("a")["href"]
-            movie_title = movie.find("h2").get("title")
-            hour_span = movie.find("span", class_="hour").text.split(" ")
+        soup = load_html_data(resource)
+        theater_movies_element = soup.find_all(
+            "ul", class_="theater-movies", attrs={"data-date": True}
+        )
+        for theater_movie in theater_movies_element:
+            date_value = theater_movie["data-date"]
+            movies = theater_movie.find_all("li", class_="")
+            for movie in movies:
+                movie_url = movie.find("a")["href"]
+                movie_title = movie.find("h2").get("title")
+                hour_span = movie.find("span", class_="hour").text.split(" ")
 
-            movie = {
-                "scheduled_date": date_value,
-                "scheduled_hour": hour_span[0],
-                "title": movie_title,
-                "language": hour_span[1],
-                "detail": movie_url,  # il faut mettre l'addres de l'api qui affiche le détail d'un movie
-            }
+                movie = Movie(
+                    **{
+                        "scheduled_date": date_value,
+                        "scheduled_hour": hour_span[0],
+                        "title": movie_title,
+                        "language": hour_span[1],
+                        "detail": movie_url,
+                    }
+                )
+                response.append(movie)
 
-            response.append(movie)
+        return response
+    except Exception as exec_info:
+        LOGGER.error(
+            "Failed to load movies for theater with slug %s. Exception %s",
+            theater,
+            str(exec_info),
+        )
+        raise ApiException(
+            status_code=HTTP_404_NOT_FOUND,
+            error_code=ApiErrorsCode.MOVIE_LOAD_FAILED,
+            message=f"Failed to load movies for theater with slug {theater}",
+        )
 
-    return response
 
-
-def load_movie_info(slug: str):
+def load_movie_info(slug: str) -> MovieDetail:
     """
     Load movie info
     :param slug:
@@ -50,97 +72,156 @@ def load_movie_info(slug: str):
     """
     resource = f"{host}movies/{slug}/"
 
-    soup = load_html_data(resource)
+    try:
+        soup = load_html_data(resource)
 
-    genre = soup.select_one(
-        "div.movie-top-container-cover-content > p.genres > span"
-    ).text.strip()
-    synopsis = soup.select_one("div.synopse-modal > p").text.strip()
-    date_time_element = soup.find(
-        "div", class_="movie-top-container-cover-content"
-    ).find("p")
-    out_date = (
-        date_time_element.find("span", class_="date")
-        .get_text(strip=True)
-        .split(":")[1]
-        .strip()
-    )
-    movie_length = (
-        date_time_element.find("span", class_="time")
-        .get_text(strip=True)
-        .split(":")[1]
-        .strip()
-    )
-    trailer = soup.select_one("div.movie > iframe").get("src")
+        genre = soup.select_one(
+            "div.movie-top-container-cover-content > p.genres > span"
+        ).text.strip()
+        synopsis = soup.select_one("div.synopse-modal > p").text.strip()
+        date_time_element = soup.find(
+            "div", class_="movie-top-container-cover-content"
+        ).find("p")
+        out_date = (
+            date_time_element.find("span", class_="date")
+            .get_text(strip=True)
+            .split(":")[1]
+            .strip()
+        )
+        movie_length = (
+            date_time_element.find("span", class_="time")
+            .get_text(strip=True)
+            .split(":")[1]
+            .strip()
+        )
+        trailer = soup.select_one("div.movie > iframe").get("src")
 
-    return {
-        "genre": genre,
-        "release_date": out_date,
-        "length": movie_length,
-        "synopsis": synopsis,
-        "trailer": trailer,
-    }
+        return MovieDetail(
+            **{
+                "genre": genre,
+                "release_date": out_date,
+                "length": movie_length,
+                "synopsis": synopsis,
+                "trailer": trailer,
+            }
+        )
+    except Exception as exec_info:
+        LOGGER.error(
+            "Failed to load movie with slug %s. Exception %s", slug, str(exec_info)
+        )
+        raise ApiException(
+            status_code=HTTP_404_NOT_FOUND,
+            error_code=ApiErrorsCode.MOVIE_LOAD_FAILED,
+            message=f"Failed to load movie with slug {slug}",
+        )
 
 
 def load_movies_per_day(theater: str):
     """
-    Affichier la liste des films par jour pour une salle
-    :param theater:
+    Load movies per day for a given theater
+    :param theater: Slug
     :return:
     """
-    response = load_movies(theater)
+    try:
+        response = load_movies(theater)
 
-    movies_by_date = defaultdict(list)
-    for movie in response:
-        scheduled_date = movie["scheduled_date"]
-        movie_data = {
-            key: value for key, value in movie.items() if key != "scheduled_date"
-        }
-        movies_by_date[scheduled_date].append(movie_data)
-    return [
-        {"scheduled_date": date, "movies": movies}
-        for date, movies in movies_by_date.items()
-    ]
-
-
-# afficher les films dans une plage de date
-# Rechercher les films
-# -le weekend, rechercher les films qui sont du weekend
-# -à partir d'une heure : recherche les films a partir de 20 h (pour un jour/ pour l'ensemble de la semaine)
-# - affichier les films de la matiné / de l'aprèm / de la nuit
-# rechercher les films par les catégories/genre
+        movies_by_date = defaultdict(list)
+        for movie in response:
+            scheduled_date = movie.scheduled_date
+            movie_data = {
+                key: value
+                for key, value in movie.model_dump().items()
+                if key != "scheduled_date"
+            }
+            movies_by_date[scheduled_date].append(movie_data)
+        return [
+            {"scheduled_date": date, "movies": movies}
+            for date, movies in movies_by_date.items()
+        ]
+    except Exception as exec_info:
+        LOGGER.exception(
+            "Failed to load movies per day for theater %s. Exception %s",
+            theater,
+            str(exec_info),
+        )
+        raise ApiException(
+            status_code=HTTP_500_INTERNAL_SERVER_ERROR,
+            error_code=ApiErrorsCode.MOVIE_LOAD_FAILED,
+            message=f"Failed to load movies par day",
+        )
 
 
 def display_movies_by_date(theater, scheduled_date):
-    response = []
-    movies = load_movies(theater)
-    filtered_movies = [
-        movie for movie in movies if movie["scheduled_date"] == scheduled_date
-    ]
+    """
+    Load movies by date
+    :param theater: theater slug
+    :param scheduled_date:
+    :return:
+    """
+    try:
+        response: List[MovieByDate] = []
+        movies = load_movies(theater)
+        filtered_movies = [
+            movie for movie in movies if movie.scheduled_date == scheduled_date
+        ]
 
-    for movie in filtered_movies:
-        element = {
-            "scheduled_hour": movie["scheduled_hour"],
-            "movie_title": movie["title"],
-            "language": movie["language"],
-            "info": movie["detail"],
-        }
-        response.append(element)
-    return response
+        for movie in filtered_movies:
+            element = MovieByDate(
+                **{
+                    "scheduled_hour": movie.scheduled_date,
+                    "title": movie.title,
+                    "language": movie.language,
+                    "detail": movie.detail,
+                }
+            )
+            response.append(element)
+        return response
+    except Exception as exec_info:
+        LOGGER.exception(
+            "Failed to load movies by date for theater %s. Exception %s",
+            theater,
+            str(exec_info),
+        )
+        raise ApiException(
+            status_code=HTTP_500_INTERNAL_SERVER_ERROR,
+            error_code=ApiErrorsCode.MOVIE_LOAD_FAILED,
+            message=f"Failed to load movies per day",
+        )
 
 
-def display_movies_by_language(theater, language):
-    response = []
-    movies = load_movies(theater)
+def display_movies_by_language(theater, language) -> List[Movie]:
+    """
+    Load movies by language
+    :param theater:
+    :param language:
+    :return:
+    """
+    try:
+        response: List[Movie] = []
+        movies = load_movies(theater)
 
-    filtered_movies = [movie for movie in movies if movie["language"] == language]
+        filtered_movies = [movie for movie in movies if movie.language == language]
 
-    for movie in filtered_movies:
-        element = {
-            "scheduled_date": movie["scheduled_date"],
-            "scheduled_hour": movie["scheduled_hour"],
-            "movie_title": movie["title"],
-            "info": movie["detail"],
-        }
-        response.append(element)
-    return response
+        for movie in filtered_movies:
+            element = Movie(
+                **{
+                    "scheduled_date": movie.scheduled_date,
+                    "scheduled_hour": movie.scheduled_hour,
+                    "title": movie.title,
+                    "detail": movie.detail,
+                    "language": movie.language,
+                }
+            )
+            response.append(element)
+        return response
+    except Exception as exec_info:
+        LOGGER.exception(
+            "Failed to load movies by language for theater %s. Exception %s",
+            theater,
+            str(exec_info),
+        )
+        raise ApiException(
+            status_code=HTTP_500_INTERNAL_SERVER_ERROR,
+            error_code=ApiErrorsCode.MOVIE_LOAD_FAILED,
+            message=f"Failed to load movies by languages",
+        )
